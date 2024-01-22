e80f64cc5dacb14f7f20592c2c594bb3695caa8b
ankush_karande <ankushkarande555@gmail.com>
2024-01-22 19:22:02 +0530

    changes in homecontroller

--- src/main/java/com/tymleaf/customer/homecontroller/HomeController.java
+++ src/main/java/com/tymleaf/customer/homecontroller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api")
+@RequestMapping("/api11")
 public class HomeController {
     @GetMapping("/firstapi")
     public String firstapi(){
