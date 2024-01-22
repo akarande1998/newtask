87a03a7fee9849b252e0d154712a522edaf738e8
ankush_karande <ankushkarande555@gmail.com>
2024-01-22 19:13:07 +0530

    first commit

---
+++ src/main/java/com/tymleaf/customer/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.tymleaf.customer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig extends WebSecurityConfigurerAdapter {
+}
